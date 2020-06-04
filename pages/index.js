74f8eec19c9566a13e070c75d3f83937395403aa
Mike Carbone <mfcbone2@gmail.com>
2020-06-04 15:09:02 -0400

    :fire: New assets, going dark

--- pages/index.js
+++ pages/index.js
@@ -26,23 +26,23 @@ const AdSection = props => {
   if (props.imgRight) {
     return (
       <Section>
-        <Box backgroundColor="#FFF" display="flex" justifyContent="space-between" alignItems="center" flexWrap="no-wrap">
-          <Box maxWidth="45%" margin="0 2rem 0 0" display="block" alignSelf="center" width="initial">
-            <Heading color="var(--black)" size={2}>{props.title}</Heading>
-            <Copy fontSize="var(--fontMed)" fontWeight="100" lineHeight="1.3" color="var(--black)">{props.copy}</Copy>
+        <Box backgroundColor="var(--black)" display="flex" justifyContent="space-between" alignItems="center" flexWrap="no-wrap">
+          <Box backgroundColor="var(--black)" maxWidth="45%" margin="0 2rem 0 0" display="block" alignSelf="center" width="initial">
+            <Heading size={2}>{props.title}</Heading>
+            <Copy fontSize="var(--fontMed)" fontWeight="100" lineHeight="1.3">{props.copy}</Copy>
           </Box>
-          <Image height="400px" width="40%" src={props.imgSrc} alt={props.imgAlt} />
+          <Image height="400px" width="40%" src={props.imgSrc} alt={props.imgAlt} objectFit="contain" />
         </Box>
       </Section>
     )
   } else {
     return (
       <Section>
-        <Box backgroundColor="#FFF" display="flex" justifyContent="space-between" alignItems="center" flexWrap="no-wrap">
-          <Image height="400px" width="40%" src={props.imgSrc} alt={props.imgAlt} />
-          <Box maxWidth="45%" margin="0 0 0 2rem" display="block" alignSelf="center" width="initial">
-            <Heading color="var(--black)" size={2}>{props.title}</Heading>
-            <Copy fontSize="var(--fontMed)" fontWeight="100" lineHeight="1.3" color="var(--black)">{props.copy}</Copy>
+        <Box backgroundColor="var(--black)" display="flex" justifyContent="space-between" alignItems="center" flexWrap="no-wrap">
+          <Image height="400px" width="40%" src={props.imgSrc} alt={props.imgAlt}  objectFit="contain"/>
+          <Box backgroundColor="var(--black)" maxWidth="45%" margin="0 0 0 2rem" display="block" alignSelf="center" width="initial">
+            <Heading size={2}>{props.title}</Heading>
+            <Copy fontSize="var(--fontMed)" fontWeight="100" lineHeight="1.3">{props.copy}</Copy>
           </Box>
         </Box>
       </Section>
@@ -69,7 +69,7 @@ export default function Home() {
           alt="Blockbuster"
         />
         <Hero
-          imgSrc="./images/knivesout1.jpg"
+          imgSrc="https://occ-0-92-1723.1.nflxso.net/dnm/api/v6/E8vDc_W8CLv7-yMQu8KMEC7Rrr8/AAAABclozqDKJM1ljO5Lcmravlkh2J_VvKsVdyTKuKg_iZxpGyjIDVu_RF4-02JaMba_mk6w4nuf4lrhInWpKH7_nakm1J3D.jpg?r=189"
           alt="Knives out movie screenshot."
           title="Blockbuster's back to the future."
           subtitle="All your favorite movies in one place."
@@ -78,28 +78,30 @@ export default function Home() {
           linkWrapper={Link}
           height="700px"
         />
-        <Wrapper padding="2rem 0">
-          <AdSection
-            imgRight
-            title="Unlimited movies. Instant access."
-            copy="Watch anywhere, any time. Sign up today and receive a bag of popcorn, on us."
-            imgSrc="./images/knivesout1.jpg"
-            imgAlt="Knives Out"
-          />
-          <AdSection
-            title="Watch anywhere and stay up to date, on the go"
-            copy="Watch anywhere, any time. Sign up today and receive a bag of popcorn, on us."
-            imgSrc="./images/knivesout1.jpg"
-            imgAlt="Knives Out"
-          />
-          <AdSection
-            imgRight
-            title="Connect & stream directly to your TV"
-            copy="Watch anywhere, any time. Sign up today and receive a bag of popcorn, on us."
-            imgSrc="./images/knivesout1.jpg"
-            imgAlt="Knives Out"
-          />
-        </Wrapper>
+        <Box backgroundColor="var(--black)">
+          <Wrapper padding="2rem 0">
+            <AdSection
+              imgRight
+              title="Unlimited movies. Instant access."
+              copy="Watch anywhere, any time. Sign up today and receive a bag of popcorn, on us."
+              imgSrc="./images/phone2.png"
+              imgAlt="Video on a phone"
+            />
+            <AdSection
+              title="Watch anywhere and stay up to date, on the go"
+              copy="Watch anywhere, any time. Sign up today and receive a bag of popcorn, on us."
+              imgSrc="./images/knivesout1.jpg"
+              imgAlt="Knives Out"
+            />
+            <AdSection
+              imgRight
+              title="Connect & stream directly to your TV"
+              copy="Watch anywhere, any time. Sign up today and receive a bag of popcorn, on us."
+              imgSrc="./images/tv1.png"
+              imgAlt="Peaky Blinders playing on a TV"
+            />
+          </Wrapper>
+        </Box>
       </main>
       <Footer linkWrapper={Link} />
       
