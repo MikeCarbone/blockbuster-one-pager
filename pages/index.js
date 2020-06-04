f72787e354ab347f38d6acff7a318367288f646e
Mike Carbone <mfcbone2@gmail.com>
2020-06-03 21:30:00 -0400

    :rocket: First push

--- pages/index.js
+++ pages/index.js
@@ -1,209 +1,109 @@
 import Head from 'next/head'
+import Link from 'next/link'
+
+import {
+  Box,
+  Copy,
+  Footer,
+  Global,
+  Header,
+  Heading,
+  Hero,
+  Image,
+  Wrapper,
+  VertSpace
+} from 'blockbuster-design-system'
+
+const Section = props => (
+  <>
+    <VertSpace size={3} />
+    {props.children}
+    <VertSpace size={3} />
+  </>
+)
+
+const AdSection = props => {
+  if (props.imgRight) {
+    return (
+      <Section>
+        <Box backgroundColor="#FFF" display="flex" justifyContent="space-between" alignItems="center" flexWrap="no-wrap">
+          <Box maxWidth="45%" margin="0 2rem 0 0" display="block" alignSelf="center" width="initial">
+            <Heading color="var(--black)" size={2}>{props.title}</Heading>
+            <Copy fontSize="var(--fontMed)" fontWeight="100" lineHeight="1.3" color="var(--black)">{props.copy}</Copy>
+          </Box>
+          <Image height="400px" width="40%" src={props.imgSrc} alt={props.imgAlt} />
+        </Box>
+      </Section>
+    )
+  } else {
+    return (
+      <Section>
+        <Box backgroundColor="#FFF" display="flex" justifyContent="space-between" alignItems="center" flexWrap="no-wrap">
+          <Image height="400px" width="40%" src={props.imgSrc} alt={props.imgAlt} />
+          <Box maxWidth="45%" margin="0 0 0 2rem" display="block" alignSelf="center" width="initial">
+            <Heading color="var(--black)" size={2}>{props.title}</Heading>
+            <Copy fontSize="var(--fontMed)" fontWeight="100" lineHeight="1.3" color="var(--black)">{props.copy}</Copy>
+          </Box>
+        </Box>
+      </Section>
+    )
+  }
+}
 
 export default function Home() {
   return (
-    <div className="container">
+    <>
       <Head>
         <title>Create Next App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;700&family=Roboto:wght@100;300;400&display=swap" rel="stylesheet" />
       </Head>
 
       <main>
-        <h1 className="title">
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className="description">
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className="grid">
-          <a href="https://nextjs.org/docs" className="card">
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className="card">
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/zeit/next.js/tree/master/examples"
-            className="card"
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className="card"
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
-        </div>
+        <Global />
+        <Header
+          buttonText="Log In"
+          buttonLink="/page"
+          linkWrapper={Link}
+          logo="./images/logo.png"
+          alt="Blockbuster"
+        />
+        <Hero
+          imgSrc="./images/knivesout1.jpg"
+          alt="Knives out movie screenshot."
+          title="Blockbuster's back to the future."
+          subtitle="All your favorite movies in one place."
+          buttonText="Subscribe now"
+          buttonLink="/page"
+          linkWrapper={Link}
+          height="700px"
+        />
+        <Wrapper padding="2rem 0">
+          <AdSection
+            imgRight
+            title="Unlimited movies. Instant access."
+            copy="Watch anywhere, any time. Sign up today and receive a bag of popcorn, on us."
+            imgSrc="./images/knivesout1.jpg"
+            imgAlt="Knives Out"
+          />
+          <AdSection
+            title="Watch anywhere and stay up to date, on the go"
+            copy="Watch anywhere, any time. Sign up today and receive a bag of popcorn, on us."
+            imgSrc="./images/knivesout1.jpg"
+            imgAlt="Knives Out"
+          />
+          <AdSection
+            imgRight
+            title="Connect & stream directly to your TV"
+            copy="Watch anywhere, any time. Sign up today and receive a bag of popcorn, on us."
+            imgSrc="./images/knivesout1.jpg"
+            imgAlt="Knives Out"
+          />
+        </Wrapper>
       </main>
-
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel Logo" className="logo" />
-        </a>
-      </footer>
-
-      <style jsx>{`
-        .container {
-          min-height: 100vh;
-          padding: 0 0.5rem;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        footer img {
-          margin-left: 0.5rem;
-        }
-
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-
-        a {
-          color: inherit;
-          text-decoration: none;
-        }
-
-        .title a {
-          color: #0070f3;
-          text-decoration: none;
-        }
-
-        .title a:hover,
-        .title a:focus,
-        .title a:active {
-          text-decoration: underline;
-        }
-
-        .title {
-          margin: 0;
-          line-height: 1.15;
-          font-size: 4rem;
-        }
-
-        .title,
-        .description {
-          text-align: center;
-        }
-
-        .description {
-          line-height: 1.5;
-          font-size: 1.5rem;
-        }
-
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family: Menlo, Monaco, Lucida Console, Liberation Mono,
-            DejaVu Sans Mono, Bitstream Vera Sans Mono, Courier New, monospace;
-        }
-
-        .grid {
-          display: flex;
-          align-items: center;
-          justify-content: center;
-          flex-wrap: wrap;
-
-          max-width: 800px;
-          margin-top: 3rem;
-        }
-
-        .card {
-          margin: 1rem;
-          flex-basis: 45%;
-          padding: 1.5rem;
-          text-align: left;
-          color: inherit;
-          text-decoration: none;
-          border: 1px solid #eaeaea;
-          border-radius: 10px;
-          transition: color 0.15s ease, border-color 0.15s ease;
-        }
-
-        .card:hover,
-        .card:focus,
-        .card:active {
-          color: #0070f3;
-          border-color: #0070f3;
-        }
-
-        .card h3 {
-          margin: 0 0 1rem 0;
-          font-size: 1.5rem;
-        }
-
-        .card p {
-          margin: 0;
-          font-size: 1.25rem;
-          line-height: 1.5;
-        }
-
-        .logo {
-          height: 1em;
-        }
-
-        @media (max-width: 600px) {
-          .grid {
-            width: 100%;
-            flex-direction: column;
-          }
-        }
-      `}</style>
-
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family: -apple-system, BlinkMacSystemFont, Segoe UI, Roboto,
-            Oxygen, Ubuntu, Cantarell, Fira Sans, Droid Sans, Helvetica Neue,
-            sans-serif;
-        }
-
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
-    </div>
+      <Footer linkWrapper={Link} />
+      
+      <style jsx>{``}</style>
+    </>
   )
 }
